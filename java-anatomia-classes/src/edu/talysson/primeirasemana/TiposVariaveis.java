cca5b48fc20592e66b12bd56059c5f6961f35971
talyssonAmorim <talysson.dra@gmail.com>
2024-09-25 14:48:13 -0300

    Adicionando Exercicio Tipos e Variaveis

---
+++ java-anatomia-classes/src/edu/talysson/primeirasemana/TiposVariaveis.java
@@ -0,0 +1,17 @@
+package edu.talysson.primeirasemana;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        /*  
+         short numeroCurto2 = numeroNormal;
+         Como a linguagem e fortemente tipada é necessario fazer um casting:
+        */
+        short numeroCurto2 = (short) numeroNormal;
+        //para declarar uma constante em java usa-se a palavra final antes de declarar o tipo:
+
+        final double VALOR_DE_PI =3.1415;
+    }
+}
