98b172a373a5e565160886e4d9d0f91694cd941b
talyssonAmorim <talysson.dra@gmail.com>
2024-10-04 19:10:14 -0300

    Execução de Desafio DIO

--- conta-banco/ContaBanco.java/src/ContaTerminal.java
+++ conta-banco/ContaBanco.java/src/ContaTerminal.java
@@ -1,5 +1,18 @@
-public class App {
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, informe o seu nome:");
+        String nomeCliente = scanner.nextLine();
+        System.out.println("Por favor, informe a sua Agencia:");
+        String agencia = scanner.nextLine();
+        System.out.println("Por favor, informe o numero de sua conta:");
+        int numero = scanner.nextInt();
+        System.out.println("Por favor, informe o seu saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá ".concat(nomeCliente) + ", obrigado por criar uma conta em nosso banco, sua agência é ".concat(agencia) + ", conta " + numero + " \ne seu saldo " + saldo + " já está disponível para saque.");
     }
 }
