8856f3867b540b44f6e0853f8c4e171d7b8b4d5d
talyssonAmorim <talysson.dra@gmail.com>
2024-10-04 12:57:38 -0300

    Alterações

--- java-anatomia-classes/src/edu/talysson/segundasemana/Leitor.java
+++ java-anatomia-classes/src/edu/talysson/segundasemana/Leitor.java
@@ -12,7 +12,14 @@ public class Leitor {
         System.out.println("Digite seu nome");
         String nome = scanner.next();
 
-        System.out.println("Digite sua altua");
+        System.out.println("Digite seu sobrenome");
         String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Olá, me chamo "+ nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos.");
+        System.out.println("Minha altura é " + altura + "cm");
     }
 }
