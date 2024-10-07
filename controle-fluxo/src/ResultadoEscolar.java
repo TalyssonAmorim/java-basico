53a5c201865cf6f3d010c65fe429dc09b965e724
talyssonAmorim <talysson.dra@gmail.com>
2024-10-07 16:41:56 -0300

    Adicionando exercicio de condição composta

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,12 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 8;
+
+        if(nota >= 7)
+        {
+            System.out.println("Parabéns! Você foi aprovado!");
+        } else {
+            System.out.println("Você foi reprovado.");
+        }
+    }
+}
