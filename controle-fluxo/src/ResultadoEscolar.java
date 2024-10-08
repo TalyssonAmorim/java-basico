9f6e5b262081154c0198888b4a7e5c742e4f871c
talyssonAmorim <talysson.dra@gmail.com>
2024-10-07 21:16:52 -0300

    Adicionando exercicio de Condicional encadeada

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,11 +1,14 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 4;
 
         if(nota >= 7)
         {
             System.out.println("Parabéns! Você foi aprovado!");
-        } else {
+        }else if (nota >= 5 && nota < 7){
+            System.out.println("Prova de Recuperação");
+        }
+         else {
             System.out.println("Você foi reprovado.");
         }
     }
